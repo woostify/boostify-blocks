5f7799e9032e974edb7bc4b476cc379c22b90ebc
vietpro123 <viet09cntt@gmail.com>
2026-03-31 07:12:46 +0700

    Fix 4.7, 4.8 sync products

--- build/block-faq/index.asset.php
+++ build/block-faq/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '8bfff38bffa9cbca37aa');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd66067e92b5a1a7cf21b');
