1f7911aed83d9cc44e5b851ca3d57ab84848c4f1
vietpro123 <viet09cntt@gmail.com>
2026-05-23 21:14:53 +0700

    Fix: Option Cart icon

--- build/block-icon-child/index.asset.php
+++ build/block-icon-child/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7254d2678adba7fb427a');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '3dc463659fa675f750b0');
