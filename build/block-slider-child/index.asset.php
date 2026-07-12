d9a0561ca942c5902caf1afefb4e1c94425b29d1
vietpro123 <viet09cntt@gmail.com>
2026-07-12 17:08:20 +0700

    try fix

--- build/block-slider-child/index.asset.php
+++ build/block-slider-child/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '966921397260bb82d02c');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '05e63a86f0e213363653');
--- src/block-products/deprecated.tsx
+++ src/block-products/deprecated.tsx
@@ -2,6 +2,7 @@ import Save__260523 from "./Save__260523";
 import Save__300523 from "./Save__300523";
 import Save__020723 from "./Save__020723";
 import Save__260626 from "./Save__260626";
+import Save__110726 from "./Save__110726";
 import Save__120726 from "./Save__120726";
 import blokc1Attrs from "./attributes";
 
@@ -26,6 +27,11 @@ const v6 = {
 	save: Save__260626,
 };
 
+const v7 = {
+	attributes: blokc1Attrs,
+	save: Save__110726,
+}
+
 // Matches content saved before `advance_zIndex` existed and before
 // typography empty-array values ([]) were normalized to empty objects ({}).
 // Must NOT default advance_zIndex, so it stays undefined (and is dropped
@@ -34,11 +40,11 @@ const legacyAttrsNoZIndex = {
 	...blokc1Attrs,
 	advance_zIndex: { type: "object" },
 };
-const v7 = {
+const v8 = {
 	attributes: legacyAttrsNoZIndex,
 	save: Save__120726,
 };
 
-const deprecated = [v7, v6, v5, v4, v3, v2, v1];
+const deprecated = [v8, v7,v6, v5, v4, v3, v2, v1];
 
 export default deprecated;
