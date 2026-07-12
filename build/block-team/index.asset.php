2ba20c506b11ae950aa84bbc2e4db956e36d5b53
vietpro123 <viet09cntt@gmail.com>
2026-07-12 17:36:30 +0700

    fix bug

--- build/block-team/index.asset.php
+++ build/block-team/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '54f63e7aca4e1cc803e3');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '5ea94d2fe12193076789');
