0e448369939e5574a10201921c92cb6cb057baaf
vietpro123 <viet09cntt@gmail.com>
2026-07-21 23:56:13 +0700

    Fix Count down block.

--- build/block-countdown/index.asset.php
+++ build/block-countdown/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e684580f4a3f24a4944e');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '895215c6aa8b223333d4');
