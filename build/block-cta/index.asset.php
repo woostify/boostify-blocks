8612ede710dac23d935c78c1f752d5352d5f5a3b
vietpro123 <viet09cntt@gmail.com>
2026-08-12 23:39:36 +0700

    add file build

--- build/block-cta/index.asset.php
+++ build/block-cta/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd20a0e571269fa8a71e8');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b0a9db667602f80e99fe');
