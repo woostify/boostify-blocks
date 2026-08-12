39ee97e11ec9a39160d2e225030a35e7eb2cadc2
vietpro123 <viet09cntt@gmail.com>
2026-08-12 23:38:18 +0700

    add file build

--- build/block-faq-child/index.asset.php
+++ build/block-faq-child/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e931e98c291c81059643');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '638cc95909e7ba0a29c2');
