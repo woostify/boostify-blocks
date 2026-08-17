e47bfe22c4ff0523e324375c3a9f4e025ded42c3
vietpro123 <viet09cntt@gmail.com>
2026-08-18 00:04:38 +0700

    Fix conflict

--- build/block-common-css/index.asset.php
+++ build/block-common-css/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks'), 'version' => 'a52e7e3d8e3c08ade25d');
+<?php return array('dependencies' => array('react', 'wp-blocks'), 'version' => 'a3e57f72104341ba56a7');
