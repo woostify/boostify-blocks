7e25c8e382dd4fd42353b0d356efff1ceaed5fc4
vietpro123 <viet09cntt@gmail.com>
2026-09-03 22:39:11 +0700

    fix conflict

--- build/block-slider-swiper/index.asset.php
+++ build/block-slider-swiper/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '32c12776569428c3358d');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '85000963464e0c81fd1e');
